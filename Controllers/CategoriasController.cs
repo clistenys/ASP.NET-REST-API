da795410f66bdf75866b5983b34f7b05fe1e4527
kdclistenys01 <clistenyseduardo26@gmail.com>
2021-03-03 18:39:38 -0300

    first commit

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria()
+            {
+                CategoriaId = 1,
+                Nome = "Notebooks"
+            },
+            new Categoria()
+            {
+                CategoriaId = 2,
+                Nome = "Monitores"
+            },
+            new Categoria()
+            {
+                CategoriaId = 3,
+                Nome = "Impressoras"
+            },
+            new Categoria()
+            {
+                CategoriaId = 4,
+                Nome = "Mouses"
+            },
+            new Categoria()
+            {
+                CategoriaId = 5,
+                Nome = "Desktops"
+            },
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
